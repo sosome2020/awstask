234d5195bd5c4ad883d4d37011e61a7e54213f07
Letebele <letebelebrightsosome@gmail.com>
2021-02-16 16:28:34 +0900

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'blogs#new'
+  resources :blogs
+  root 'blogs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
